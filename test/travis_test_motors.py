435c2f71fb2b6945e41e59f80e0a1a5c434f6452
yeym190130 <masaya.yamamoto@youtheng.co.jp>
2019-02-14 04:29:12 +0000

    test motor try2

--- test/travis_test_motors.py
+++ test/travis_test_motors.py
@@ -6,7 +6,7 @@ import time
 from pimouse_ros.msg import MotorFreqs
 from geometry_msgs.msg import Twist
 
-class motorTest(unittest.TestCase):
+class MotorTest(unittest.TestCase):
 	def file_check(self,dev,value,message):
 		with open("/dev/","r") as f:
 			self.assertEqual(f.readline(),str(value)+"\n",message)
