fa9c4802891f312fa2e65a96b8d37d84c431742a
yeym190130 <masaya.yamamoto@youtheng.co.jp>
2019-02-14 06:14:41 +0000

    test motor onoff try2

--- test/travis_test_motors.py
+++ test/travis_test_motors.py
@@ -65,7 +65,7 @@ class MotorTest(unittest.TestCase):
                 self.assertEqual(ret.message,"ON","motor on wrong message")
                 with open("/dev/rtmotoren0","r") as f:
                         data=f.readline()
-                        self.assertEqual(data,"0\n","wrong value in rtmotor0 at motor on")
+                        self.assertEqual(data,"1\n","wrong value in rtmotor0 at motor on")
 
 if __name__=='__main__':
 	#time.sleep(3)
