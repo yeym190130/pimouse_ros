931379045c0a520412b3748cd06c8befad22677c
yeym190130 <masaya.yamamoto@youtheng.co.jp>
2019-02-14 06:55:12 +0000

    test motor 3 try2

--- test/travis_test_motors.py
+++ test/travis_test_motors.py
@@ -72,7 +72,7 @@ class MotorTest(unittest.TestCase):
 	def test_put_value_timed(self):
 		tm = rospy.ServiceProxy('/timed_motion',TimedMotion)
 		tm(-321,654,1500)
-		with open("/dev/rtmotor0","w") as f:
+		with open("/dev/rtmotor0","r") as f:
 			data = f.readline()
 			self.assertEqual(data,"-321 654 1500\n","value does not written t0 rtmotor0")
 
