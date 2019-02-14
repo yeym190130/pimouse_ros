5a744284a1aa0f951bf48ef9aa86523e27b93a69
yeym190130 <masaya.yamamoto@youtheng.co.jp>
2019-02-14 04:52:39 +0000

    test motor try6

--- test/travis_test_motors.py
+++ test/travis_test_motors.py
@@ -28,7 +28,7 @@ class MotorTest(unittest.TestCase):
 		self.file_check("rtmotor_raw_r0",m.right_hz,"wrong right value from motor_raw_r0")
 
 	def test_put_cmd_vel(self):
-		pub=self.Publisher('/cmd_vel',Twist)
+		pub=rospy.Publisher('/cmd_vel',Twist)
 		m=Twist()
 		m.linear.x=0.1414
 		m.angular.z=1.57
